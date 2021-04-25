c2ee528d4aa29ef0621287801459e5a00735f31c
Sara Ajana <sara.ajana@kedgebs.com>
2021-04-25 19:42:03 +0200

    quest done

--- cart.php
+++ cart.php
@@ -1,7 +1,23 @@
 <?php require 'inc/head.php'; ?>
+<?php require 'inc/data/products.php'; ?>
+
+<?php
+if (!isset($_SESSION['username'])) {
+    header('Location: /login.php');
+}
+?>
 <section class="cookies container-fluid">
     <div class="row">
-        TODO : Display shopping cart items from $_SESSION here.
+
+        <ul>
+            <?php foreach ($_SESSION['cookies'] as $cookiesId => $quantity) : ?>
+                <li>
+                    <?php echo $catalog[$cookiesId]['name']; ?> : <?= $quantity ?>
+                    <br>
+                    <?php echo $catalog[$cookiesId]['description']; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
--- index.php
+++ index.php
@@ -1,5 +1,15 @@
+
 <?php require 'inc/data/products.php'; ?>
 <?php require 'inc/head.php'; ?>
+
+<?php
+if (isset($_GET['add_to_cart'])) {
+    $id = $_GET['add_to_cart'];
+    $_SESSION['cookies'][$id] += 1;
+    isset($_SESSION['cookies']) ? $_SESSION['cookies'] += 1 : $_SESSION['cookies'] = 1;
+}
+?>
+
 <section class="cookies container-fluid">
     <div class="row">
         <?php foreach ($catalog as $id => $cookie) { ?>
